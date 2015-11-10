c8e38b169418e8675fb6a492de4035dbaf155788
Bc. Vojta Tosovsky <tosovvoj@fit.cvut.cz>
2015-11-10 10:30:16 +0100

    Four Task set

---
+++ ProgramingBasic/src/four/fourTaskSet.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package four;
+
+/**
+ *
+ * @author Vojtech Tosovsky
+ */
+public class fourTaskSet {
+
+//	vrací mocninu čísla
+//	pro vstup 10,3 vrací 100
+//	pro vstup 10,4 vrací 100
+//	pro vstup 2,2 vrací 4
+//	pro vstup 3,2 vrací 9
+//	pro vstup 8,0 vrací 1
+//	pro vstup 8,1 vrací 8
+	public static int power(int base, int exponent) {
+		return -10;
+
+	}
+
+	// vrací true, pokud je zadané číslo prvočíslo, jinak vrací false;
+//	;priklady
+	//pro 1 vrátí true
+	//pro 2 vrátí true
+	//pro 3 vrátí true
+	//pro 4 vrátí false
+	//pro 11 vrátí true
+	//pro 128 vrátí false
+	public static boolean prime(int n) {
+		return true;
+
+	}
+
+//	Vrací počet prvočísel menších než n.
+//	pro 2 vrací 1 (je to jen jednička)
+//	pro 3 vrací 2 (je to jednička a dvojka)
+//	pro 4 vrací 3 (1,2,3)
+//	pro 5 vrací 3 (1,2,3)
+//	pro 6 vrací 4 (1,2,3,5)
+//	pro 7 vrací 4 (1,2,3,5)
+//	pro 8 vrací 5 (1,2,3,5,7)
+//	pro 12 vrací 6 (1,2,3,5,7,11)
+	public static int primeCountUnderN(int n) {
+		return 0;
+
+	}
+
+}
